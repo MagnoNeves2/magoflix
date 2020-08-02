5d417b4760f1b53e8b52c924225bcbbded40722c
Magno <magnoneves2@gmail.com>
2020-08-02 02:54:55 -0300

    UPDATE: dados inicias - modificação dos dados fornecidos

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-End? Trabalhando na área"}
+        videoDescription={"Mais uma noite sem dormir Vou lá pra rua espairecer Pensar na vida refletir Meu Deus o que posso fazer Abro a janela e não vejo o sol brilhar Abro a carteira tem nada pra contar Mó desespero dois filho pra criar Ando esquecido do lado de cá"}
       />
 
       <Carousel
